e0826a95113ce41d8fe95c5a7b77decb740d6daa
TheOpenBit <tob@schoenesnetz.de>
2016-06-11 13:40:34 +0200

    read log of remote master branch

--- gitpull.sh
+++ gitpull.sh
@@ -1,11 +1,12 @@
 #/bin/bash 
 
 set -e 
+git fetch
 if [ -f ~/lastcommit ]; then
    lastcommit=$(cat ~/lastcommit)
    
 fi
-currentcommit=$(git log -1 | grep commit)
+currentcommit=$(git log -1 origin/master | grep commit)
 if [ "$currentcommit" != "$lastcommit" ]; then
   git pull
   echo $currentcommit > ~/lastcommit
